bda952e08c28cdb87d9abb19e1aad76d33a376b4
Ying Zhou <yingzhou@thoughtworks.com>
2018-03-05 10:50:08 +0800

    create conv_3x3_layer in forward propagation

--- src/forward_propagation.py
+++ src/forward_propagation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 
@@ -5,4 +6,13 @@ def conv_1x1_layer(x, w):
     assert w.shape[0] == 1
     assert w.shape[1] == 1
     z = tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
+    return z
+
+
+def conv_3x3_layer(x, w):
+    assert w.shape[0] == 3
+    assert w.shape[1] == 3
+    m = x.shape[-1]
+    z = conv_1x1_layer(x, np.ones((1, 1, m, 16)))
+    z = tf.nn.conv2d(z, w, strides=[1, 1, 1, 1], padding='SAME')
     return z
--- test/test_forward_propagation.py
+++ test/test_forward_propagation.py
@@ -19,6 +19,22 @@ class ForwardPropagationTest(unittest.TestCase):
             z = sess.run(conv_1x1_layer(x, w))
         self.assertTrue((np.sum(x, keepdims=True, axis=-1) == z).all())
 
+    def test_exceptions_of_conv_3x3_layer(self):
+        from src.forward_propagation import conv_3x3_layer
+        x = np.random.randn(2, 24, 24, 3)
+        w = np.random.randn(1, 1, 3, 3)
+        self.assertRaises(AssertionError, conv_3x3_layer, x, w)
+
+    def test_result_of_conv_3x3_layer(self):
+        from src.forward_propagation import conv_3x3_layer
+        with tf.Session() as sess:
+            x = sess.run(tf.cast(np.ones((1, 3, 3, 1)), tf.float32))
+            w = sess.run(tf.cast(np.ones((3, 3, 16, 1)), tf.float32))
+            z = sess.run(conv_3x3_layer(x, w))
+        self.assertTrue(([[[[16 * 4], [16 * 6], [16 * 4]],
+                           [[16 * 6], [16 * 9], [16 * 6]],
+                           [[16 * 4], [16 * 6], [16 * 4]]]] == z).all())
+
 
 if __name__ == '__main__':
     unittest.main()
