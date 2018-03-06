7d9927faef4b7a89df2c7cd8164f23d6c452ebfd
Ying Zhou <yingzhou@thoughtworks.com>
2018-03-06 10:04:33 +0800

    initialize parameters

--- src/initialization.py
+++ src/initialization.py
@@ -1,5 +1,7 @@
 import pickle
 
+import tensorflow as tf
+
 
 def unpickle(file):
     with open(file, 'rb') as fo:
@@ -33,3 +35,23 @@ def get_test_set():
 
 def format_x_data(x):
     return x.reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
+
+
+def initialize_parameters():
+    tf.set_random_seed(1)
+    w11 = tf.get_variable('w11', [1, 1, 128, 64], initializer=tf.contrib.layers.xavier_initializer())
+    w13 = tf.get_variable('W13', [3, 3, 16, 32], initializer=tf.contrib.layers.xavier_initializer())
+    w15 = tf.get_variable('w15', [5, 5, 16, 16], initializer=tf.contrib.layers.xavier_initializer())
+    w21 = tf.get_variable('w21', [1, 1, 128, 64], initializer=tf.contrib.layers.xavier_initializer())
+    w23 = tf.get_variable('W23', [3, 3, 16, 32], initializer=tf.contrib.layers.xavier_initializer())
+    w25 = tf.get_variable('w25', [5, 5, 16, 16], initializer=tf.contrib.layers.xavier_initializer())
+
+    parameters = {'w11': w11,
+                  'W13': w13,
+                  'w15': w15,
+                  'w21': w21,
+                  'W23': w23,
+                  'w25': w25
+                  }
+
+    return parameters
--- test/test_initialization.py
+++ test/test_initialization.py
@@ -13,6 +13,16 @@ class InitializationTest(unittest.TestCase):
         self.assertEqual(x[0][1], x_formatted[0][0][1][0])
         self.assertEqual(x[0][1024], x_formatted[0][0][0][1])
 
+    def test_result_of_initialize_parameters(self):
+        from src.initialization import initialize_parameters
+        parameters = initialize_parameters()
+        self.assertEqual((1, 1, 128, 64), parameters['w11'].shape)
+        self.assertEqual((3, 3, 16, 32), parameters['W13'].shape)
+        self.assertEqual((5, 5, 16, 16), parameters['w15'].shape)
+        self.assertEqual((1, 1, 128, 64), parameters['w21'].shape)
+        self.assertEqual((3, 3, 16, 32), parameters['W23'].shape)
+        self.assertEqual((5, 5, 16, 16), parameters['w25'].shape)
+
 
 if __name__ == '__main__':
     unittest.main()
