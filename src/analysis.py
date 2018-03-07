c56e00c7108ee13af7c9deabb8253cf838941055
Ying Zhou <yingzhou@thoughtworks.com>
2018-03-07 14:12:44 +0800

    create function to clean log folder

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,6 @@
+import shutil
+
+import os
 import tensorflow as tf
 
 
@@ -5,3 +8,8 @@ def compute_accuracy(z, y):
     z = tf.argmax(z, axis=-1)
     y = tf.argmax(y, axis=-1)
     return tf.reduce_mean(tf.cast(tf.equal(z, y), tf.float32))
+
+
+def clean_log_folder():
+    shutil.rmtree('log')
+    os.mkdir('log')
