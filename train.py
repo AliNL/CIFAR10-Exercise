60b6c357dff1a368dd512e1df6d1807e1fbe81ae
Ying Zhou <yingzhou@thoughtworks.com>
2018-03-07 14:13:54 +0800

    add graph and clean log

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import datetime
 
 import os
 
-from src.analysis import compute_accuracy
+from src.analysis import compute_accuracy, clean_log_folder
 from src.forward_propagation import *
 from src.initialization import *
 
@@ -10,6 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 def train(learning_rate=0.001, mini_batch_size=512, number_of_steps=100):
+    clean_log_folder()
     tf.reset_default_graph()
 
     x_train, y_train = get_training_set(1)
@@ -54,8 +55,8 @@ def train(learning_rate=0.001, mini_batch_size=512, number_of_steps=100):
             else:
                 train_summary, c, ac, _ = sess.run([merged_summary_op, cost, accuracy, optimizer],
                                                    feed_dict={x: x_train, y: y_train})
-                train_writer.add_summary(train_summary, step - 1)
-                train_writer.flush()
+            train_writer.add_summary(train_summary, step - 1)
+            train_writer.flush()
 
             if step % 10 == 0:
                 if step:
@@ -74,6 +75,7 @@ def train(learning_rate=0.001, mini_batch_size=512, number_of_steps=100):
                 test_writer.flush()
                 print('=' * 100)
 
+        train_writer.add_graph(sess.graph)
     print('Finish time:', datetime.datetime.now())
 
 
