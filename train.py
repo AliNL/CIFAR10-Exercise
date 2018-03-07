87e9db5cdd325efe5f906d509c9ab0abe5333ec8
Ying Zhou <yingzhou@thoughtworks.com>
2018-03-07 10:27:02 +0800

    add accuracy calculation into training model

--- train.py
+++ train.py
@@ -1,12 +1,15 @@
+import datetime
+
 import os
 
+from src.analysis import compute_accuracy
 from src.forward_propagation import *
 from src.initialization import *
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-def train(learning_rate=0.001, minibatch_size=500, number_of_steps=50, mini_batch=False):
+def train(learning_rate=0.001, mini_batch=False, minibatch_size=512, number_of_steps=100):
     tf.reset_default_graph()
 
     x_train, y_train = get_training_set(1)
@@ -26,6 +29,9 @@ def train(learning_rate=0.001, minibatch_size=500, number_of_steps=50, mini_batc
     cost = compute_cost(z, y)
     tf.summary.scalar("cost", cost)
 
+    accuracy = compute_accuracy(z, y)
+    tf.summary.scalar("accuracy", accuracy)
+
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
     with tf.Session() as sess:
@@ -35,6 +41,8 @@ def train(learning_rate=0.001, minibatch_size=500, number_of_steps=50, mini_batc
         test_writer = tf.summary.FileWriter('log/plot_3')
         sess.run(tf.global_variables_initializer())
 
+        start_time = datetime.datetime.now()
+        print('Start time:', start_time)
         for step in range(number_of_steps):
             if mini_batch:
                 mini_batches = random_mini_batches(x_train, y_train, minibatch_size)
@@ -44,19 +52,24 @@ def train(learning_rate=0.001, minibatch_size=500, number_of_steps=50, mini_batc
             else:
                 sess.run(optimizer, feed_dict={x: x_train, y: y_train})
 
-            train_error = sess.run(merged_summary_op, feed_dict={x: x_train, y: y_train})
-            train_writer.add_summary(train_error, step)
-            train_writer.flush()
+            if step % 10 == 0:
+                train_error = sess.run(merged_summary_op, feed_dict={x: x_train, y: y_train})
+                train_writer.add_summary(train_error, step)
+                train_writer.flush()
 
-            dev_error = sess.run(merged_summary_op, feed_dict={x: x_dev, y: y_dev})
-            dev_writer.add_summary(dev_error, step)
-            dev_writer.flush()
+                dev_error = sess.run(merged_summary_op, feed_dict={x: x_dev, y: y_dev})
+                dev_writer.add_summary(dev_error, step)
+                dev_writer.flush()
 
-            test_error = sess.run(merged_summary_op, feed_dict={x: x_test, y: y_test})
-            test_writer.add_summary(test_error, step)
-            test_writer.flush()
-            print(step)
+                test_error = sess.run(merged_summary_op, feed_dict={x: x_test, y: y_test})
+                test_writer.add_summary(test_error, step)
+                test_writer.flush()
+
+                print('step:', step)
+                print('seconds per step: ', (datetime.datetime.now() - start_time).total_seconds() / step)
+                print('=' * 100)
+    print('Finish time:', datetime.datetime.now())
 
 
 if __name__ == '__main__':
-    train()
+    train(learning_rate=0.001, mini_batch=False, number_of_steps=100)
